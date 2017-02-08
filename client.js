362e4e3bc2385b828b8c4d67a9dc400ddceb981b
Catherine J <cjarocki@gmail.com>
2017-02-08 17:36:24 -0600

    first commit

---
+++ client.js
@@ -0,0 +1,13 @@
+function numFunction(numberFirst, numberSecond) {
+
+  if (numberFirst > numberSecond) {
+  console.log("The first number was bigger!");
+  } else if (numberSecond > numberFirst) {
+  console.log("The second number was bigger!");
+  } else if (numberFirst == numberSecond) {
+  console.log("The numbers are the same!");
+  }
+
+  }
+
+ numFunction(8,7);
